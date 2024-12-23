d355161e6294cf0649e06e5903a34ff4d2207bc3
Ivan <vanechka-nikitin-2004@mail.ru>
2024-12-24 00:56:09 +0800

    Add logic for comments

--- project/cars/api_views.py
+++ project/cars/api_views.py
@@ -27,6 +27,27 @@ class CarViewSet(viewsets.ModelViewSet):
         # При создании нового автомобиля установить owner = текущий пользователь
         serializer.save(owner=self.request.user)
 
+    @action(detail=True, methods=['get', 'post'])
+    def comments(self, request, pk=None):
+        """
+        GET /api/cars/<id>/comments/  - получить все комментарии к машине
+        POST /api/cars/<id>/comments/ - добавить новый комментарий к машине
+        """
+        car = get_object_or_404(Car, pk=pk)
+        if request.method == 'GET':
+            comments = car.comments.all()
+            serializer = CommentSerializer(comments, many=True)
+            return Response(serializer.data)
+
+        elif request.method == 'POST':
+            if not request.user.is_authenticated:
+                return Response({'error': 'Вы должны быть авторизованы.'}, status=401)
+            serializer = CommentSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save(author=request.user, car=car)
+                return Response(serializer.data, status=201)
+            return Response(serializer.errors, status=400)
+
 
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
