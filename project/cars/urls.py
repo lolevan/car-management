e096c9087f7b6c747fb9b2c5e579c265a93b4c73
Ivan <vanechka-nikitin-2004@mail.ru>
2024-12-24 00:47:48 +0800

    Add urls

---
+++ project/cars/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from .views import (
+    car_list,
+    car_detail,
+    car_create,
+    car_update,
+    car_delete,
+)
+
+
+urlpatterns = [
+    path('', car_list, name='car_list'),
+    path('cars/<int:car_id>/', car_detail, name='car_detail'),
+    path('cars/create/', car_create, name='car_create'),
+    path('cars/<int:car_id>/edit/', car_update, name='car_update'),
+    path('cars/<int:car_id>/delete/', car_delete, name='car_delete'),
+]
